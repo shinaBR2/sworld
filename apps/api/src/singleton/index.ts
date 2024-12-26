2d292072ba35df417a4aa19ab01d313636a4f38a
vuong <shinabr2@gmail.com>
2024-12-26 19:03:12 +0700

    remove api

--- apps/api/src/singleton/index.ts
+++
@@ -1,5 +0,0 @@
-import db from './db';
-import { onCall, onRequest, onRequestWithCors, getTimeStamp } from './request';
-
-export default db;
-export { onCall, onRequest, onRequestWithCors, getTimeStamp };
