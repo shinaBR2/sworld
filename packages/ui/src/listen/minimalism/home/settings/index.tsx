25217ddefe2b7f7c5372cc858dc7116f6cdbd375
vuong <shinabr2@gmail.com>
2024-12-27 08:32:09 +0700

    Settings for listen

---
+++ packages/ui/src/listen/minimalism/home/settings/index.tsx
@@ -0,0 +1,51 @@
+import { Logout } from '@mui/icons-material';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+interface SettingsPanelProps {
+  open: boolean;
+  toggle: React.Dispatch<React.SetStateAction<boolean>>;
+  actions: {
+    logout: () => void;
+  };
+}
+
+const texts = {
+  logout: 'Logout',
+};
+
+const SettingsPanel = (props: SettingsPanelProps) => {
+  const { open, toggle, actions } = props;
+  const { logout } = actions;
+
+  return (
+    <Drawer anchor="right" open={open} onClose={() => toggle(false)}>
+      <Box
+        sx={{
+          width: 250,
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <List sx={{ flex: 1 }}></List>
+        <Divider />
+        <List>
+          <ListItemButton onClick={logout}>
+            <ListItemIcon>
+              <Logout />
+            </ListItemIcon>
+            <ListItemText primary={texts.logout} />
+          </ListItemButton>
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
+
+export { SettingsPanel };
--- packages/ui/src/listen/minimalism/index.ts
+++ packages/ui/src/listen/minimalism/index.ts
@@ -4,11 +4,13 @@ import { Header } from './header';
 import { FeelingList } from './home/feeling-list';
 import { AudioList } from './home/audio-list';
 import { MainContainer } from './home/main-container';
+import { SettingsPanel } from './home/settings';
 
 export {
   MinimalismThemeProvider,
   MusicWidget,
   Header,
+  SettingsPanel,
   FeelingList,
   AudioList,
   MainContainer,
