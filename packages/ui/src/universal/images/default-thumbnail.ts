c6d3f98da88b0ae8129141a9b8efcdbfdc9b750f
vuong <shinabr2@gmail.com>
2024-12-29 08:20:44 +0700

    optimize svg

--- packages/ui/src/universal/images/default-thumbnail.ts
+++ packages/ui/src/universal/images/default-thumbnail.ts
@@ -1,19 +1,7 @@
-const defaultThumbnailUrl = `data:image/svg+xml,${encodeURIComponent(`
-  <svg viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg">
-    <rect width="1920" height="1080" fill="#f0f0f0"/>
-    <circle cx="960" cy="540" r="100" fill="#e0e0e0"/>
-    <path d="M920 480 L1020 540 L920 600 Z" fill="#9e9e9e"/>
-    <path d="M0 200 Q 480 400 960 200 T 1920 200" stroke="#e8e8e8" fill="none" stroke-width="40"/>
-    <path d="M0 800 Q 480 600 960 800 T 1920 800" stroke="#e8e8e8" fill="none" stroke-width="40"/>
-  </svg>
-  `)}`;
+// prettier-ignore
+const defaultThumbnailUrl = `data:image/svg+xml,${encodeURIComponent(`<svg viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg"><rect width="1920" height="1080" fill="#f0f0f0"/><circle cx="960" cy="540" r="100" fill="#e0e0e0"/><path d="M920 480 L1020 540 L920 600 Z" fill="#9e9e9e"/><path d="M0 200 Q 480 400 960 200 T 1920 200" stroke="#e8e8e8" fill="none" stroke-width="40"/><path d="M0 800 Q 480 600 960 800 T 1920 800" stroke="#e8e8e8" fill="none" stroke-width="40"/></svg>`)}`;
 
-const defaultAudioThumbnailUrl = `data:image/svg+xml,${encodeURIComponent(`
-  <svg viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg">
-    <rect width="1920" height="1080" fill="#f0f0f0"/>
-    <path d="M0 200 Q 480 400 960 200 T 1920 200" stroke="#e8e8e8" fill="none" stroke-width="40"/>
-    <path d="M0 800 Q 480 600 960 800 T 1920 800" stroke="#e8e8e8" fill="none" stroke-width="40"/>
-  </svg>
-  `)}`;
+// prettier-ignore
+const defaultAudioThumbnailUrl = `data:image/svg+xml,${encodeURIComponent(`<svg viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg"><rect width="1920" height="1080" fill="#f0f0f0"/><path d="M0 200 Q 480 400 960 200 T 1920 200" stroke="#e8e8e8" fill="none" stroke-width="40"/><path d="M0 800 Q 480 600 960 800 T 1920 800" stroke="#e8e8e8" fill="none" stroke-width="40"/></svg>`)}`;
 
 export { defaultThumbnailUrl, defaultAudioThumbnailUrl };
