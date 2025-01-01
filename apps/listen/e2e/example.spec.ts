1578756fda46281b012d0d132601e82afa4a44b7
vuong <shinabr2@gmail.com>
2025-01-01 10:25:42 +0700

    test first flow

--- apps/listen/e2e/example.spec.ts
+++ apps/listen/e2e/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title', async ({ page, baseURL }) => {
+  console.log(`baseURL`, baseURL);
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -14,5 +15,7 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'Installation' })
+  ).toBeVisible();
 });
