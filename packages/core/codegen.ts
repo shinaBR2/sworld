dbb9dd38b3952f2d444ebb5562d1bc8a45a3da81
vuong <shinabr2@gmail.com>
2025-01-05 20:29:45 +0700

    clean

--- packages/core/codegen.ts
+++ packages/core/codegen.ts
@@ -7,8 +7,7 @@ const config: CodegenConfig = {
     {
       'https://relieved-panther-58.hasura.app/v1/graphql': {
         headers: {
-          'x-hasura-admin-secret':
-            'W6OP2ZTd9CubvmoI0xjLzQlnd71f2JL7g5exoNJUfNU58gxeSRG6WX2x5K26yr5g',
+          'x-hasura-admin-secret': 'REPLACE_ADMIN_SECRET',
         },
       },
     },
