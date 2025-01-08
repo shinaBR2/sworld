ddef5da988082aa934f64a83719a1dd49462a255
vuong <shinabr2@gmail.com>
2025-01-08 08:09:01 +0700

    remove background

--- packages/ui/src/watch/videos/video-player/index.tsx
+++ packages/ui/src/watch/videos/video-player/index.tsx
@@ -21,7 +21,6 @@ const VideoPlayer = ({ video }: { video: Video }) => {
         height={isMobile ? '100%' : 'auto'}
         style={{
           aspectRatio: '16/9',
-          backgroundColor: '#e0e0e0',
         }}
         light={thumbnailUrl ?? defaultThumbnailUrl}
         onError={(error: unknown) => {
