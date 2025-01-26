658aee4334e7cdd1b00216f2a451576822362fb1
vuong <shinabr2@gmail.com>
2025-01-26 08:37:57 +0700

    Fix listen development

--- apps/listen/src/main.tsx
+++ apps/listen/src/main.tsx
@@ -3,27 +3,28 @@ import { ErrorBoundary } from 'core';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import {
-  auth0Config,
-  queryConfig,
-  sentryConfig,
-  validateEnvVars,
-} from './config';
+import { auth0Config, queryConfig, sentryConfig, validateEnvVars } from './config';
 import { ErrorFallback } from 'ui/universal/error-boundary';
+import { MinimalismThemeProvider } from 'ui/listen/minimalism';
 
 validateEnvVars();
 initSentry(sentryConfig);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+/**
+ * Put MinimalismThemeProvider under App component will cause the error:
+ * Uncaught TypeError: createTheme_default is not a function
+ */
 
 root.render(
   <React.StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <Auth.AuthProvider config={auth0Config}>
         <Query.QueryProvider config={queryConfig}>
-          <App />
+          <MinimalismThemeProvider>
+            <App />
+          </MinimalismThemeProvider>
         </Query.QueryProvider>
       </Auth.AuthProvider>
     </ErrorBoundary>
