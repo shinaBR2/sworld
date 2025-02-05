dc3c25aa289120c94dd37e53c4bee981c176d950
vuong <shinabr2@gmail.com>
2025-02-05 17:12:08 +0700

    First working on dev

--- packages/ui/src/watch/home-page/settings/upload-button/index.tsx
+++ packages/ui/src/watch/home-page/settings/upload-button/index.tsx
@@ -3,24 +3,28 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Skeleton from '@mui/material/Skeleton';
-import { Query } from 'core';
+import { Auth, Query, watchMutationHooks } from 'core';
 import { lazy, Suspense, useState } from 'react';
 
 const VideoUploadDialog = lazy(() => import('../../../dialogs/upload'));
+const { useBulkConvertVideos } = watchMutationHooks;
 
 const UploadButton = () => {
   const [open, setOpen] = useState(false);
+  const { user, getAccessToken } = Auth.useAuthContext();
   const { featureFlags } = Query.useQueryContext();
+  const { mutate: bulkConvert } = useBulkConvertVideos({
+    getAccessToken,
+    onSuccess: data => {
+      console.log('Videos converted:', data.insert_videos.returning);
+    },
+  });
+
   const { data, isLoading } = featureFlags;
 
   if (isLoading) {
     return (
-      <ListItemButton
-        disabled
-        aria-disabled="true"
-        aria-label="Upload button loading"
-        role="button"
-      >
+      <ListItemButton disabled aria-disabled="true" aria-label="Upload button loading" role="button">
         <ListItemIcon>
           <Skeleton
             variant="circular"
@@ -30,15 +34,7 @@ const UploadButton = () => {
             data-testid="upload-button-skeleton-icon"
           />
         </ListItemIcon>
-        <ListItemText
-          primary={
-            <Skeleton
-              width={80}
-              aria-hidden="true"
-              data-testid="upload-button-skeleton-text"
-            />
-          }
-        />
+        <ListItemText primary={<Skeleton width={80} aria-hidden="true" data-testid="upload-button-skeleton-text" />} />
       </ListItemButton>
     );
   }
@@ -66,7 +62,23 @@ const UploadButton = () => {
             open={open}
             onOpenChange={setOpen}
             onSubmit={async urls => {
+              if (!enabled) {
+                return;
+              }
+
               console.log(`submit url`, urls);
+              bulkConvert({
+                objects: urls.map((url, index) => {
+                  const now = Date.now();
+
+                  return {
+                    title: `untitled-${now}`,
+                    description: `description-${now} for url ${url}`,
+                    slug: `untitled-${user?.id}-${index}-${now}`,
+                    video_url: url,
+                  };
+                }),
+              });
             }}
           />
         </Suspense>
