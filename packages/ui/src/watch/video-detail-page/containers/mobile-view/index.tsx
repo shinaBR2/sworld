6878fa4b8529ace3d5dea11c86035e880fe2b96d
vuong <shinabr2@gmail.com>
2025-02-08 13:59:39 +0700

    styling

--- packages/ui/src/watch/video-detail-page/containers/mobile-view/index.tsx
+++ packages/ui/src/watch/video-detail-page/containers/mobile-view/index.tsx
@@ -17,6 +17,8 @@ const styles = {
   videoContainer: {
     width: '100%',
     height: VIDEO_ASPECT_RATIO,
+    px: 2,
+    py: 2,
   },
   scrollableList: {
     overflow: 'auto',
@@ -42,7 +44,7 @@ const SKELETON_ITEMS_COUNT = 5;
 const LoadingSkeleton = () => (
   <Grid container direction="row" sx={styles.container} aria-busy="true">
     <Grid sx={styles.videoContainer}>
-      <Skeleton variant="rectangular" width="100%" height="100%" animation="wave" />
+      <Skeleton variant="rectangular" width="100%" height="100%" animation="wave" sx={{ borderRadius: 1 }} />
     </Grid>
     <Grid item xs={12}>
       <Skeleton
