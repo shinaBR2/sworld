b6fe2cafb10952bba99f4cdec16a04da2d5614e4
vuong <shinabr2@gmail.com>
2025-02-21 21:03:22 +0700

    Build core same as ui

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  // this dts thing only work if max_old_space_size set
+  dts: true,
+  sourcemap: true,
+  entry: ['./src/**/*.ts?(x)', '!./src/**/*.test.ts?(x)', '!./src/**/*.stories.ts?(x)'],
+  format: ['esm'],
+  outDir: 'dist/',
+  treeshake: true,
+  splitting: true,
+  shims: true,
+  external: ['react'],
+});
