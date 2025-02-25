27cb9e9e16cb102dd56956071975c06812013770
vuong <shinabr2@gmail.com>
2025-02-25 07:37:24 +0700

    fix ts

--- packages/ui/src/watch/video-detail-page/containers/types.ts
+++ packages/ui/src/watch/video-detail-page/containers/types.ts
@@ -1,8 +1,9 @@
 import { useLoadPlaylistDetail } from 'core/watch/query-hooks/playlist-detail';
 import { RequiredLinkComponent } from '../../videos/types';
+import { useLoadVideoDetail } from 'core/watch/query-hooks/video-detail';
 
 interface VideoDetailContainerProps extends Omit<RequiredLinkComponent, 'linkProps'> {
-  queryRs: ReturnType<typeof useLoadPlaylistDetail>;
+  queryRs: ReturnType<typeof useLoadPlaylistDetail> | ReturnType<typeof useLoadVideoDetail>;
   activeVideoId: string;
 }
 
