52288b9e1aaf3b6f9491246382be9e13b9ef581e
vuong <shinabr2@gmail.com>
2025-02-25 07:54:34 +0700

    fix test

--- packages/core/src/watch/routes/index.test.ts
+++ packages/core/src/watch/routes/index.test.ts
@@ -1,54 +1,38 @@
 import { describe, it, expect } from 'vitest';
-import { generateVideoDetailRoute } from './index';
-import { MEDIA_TYPES, TransformedMediaItem } from '../query-hooks';
+import { generateVideoDetailRoute, generateVideoInPlaylistRoute } from './index';
 
 describe('generateVideoDetailRoute', () => {
-  it('should return video route for video type', () => {
-    const video = {
+  it('should return video route', () => {
+    const props = {
       id: '123',
-      type: MEDIA_TYPES.VIDEO,
-      title: 'Test Video',
       slug: 'test-video',
-    } as TransformedMediaItem;
+    };
 
-    expect(generateVideoDetailRoute(video)).toEqual({
-      to: '/video/$slug-$id',
+    expect(generateVideoDetailRoute(props)).toEqual({
+      to: '/video/$slug/$id',
       params: {
         slug: 'test-video',
         id: '123',
       },
     });
   });
+});
 
-  it('should return playlist route for playlist type', () => {
-    const video = {
-      id: '123',
-      type: MEDIA_TYPES.PLAYLIST,
-      title: 'Test Playlist',
-      slug: 'test-playlist',
-    } as TransformedMediaItem;
+describe('generateVideoInPlaylistRoute', () => {
+  it('should return video route for video in a playlist', () => {
+    const props = {
+      playlistId: 'playlist-abc',
+      playlistSlug: 'playlist-slug',
+      videoId: 'video-123',
+    };
 
-    expect(generateVideoDetailRoute(video)).toEqual({
-      to: '/playlist/$slug-$id',
+    expect(generateVideoInPlaylistRoute(props)).toEqual({
+      to: '/playlist/$slug/$playlistId/$videoId',
       params: {
-        slug: 'test-playlist',
-        id: '123',
+        slug: 'playlist-slug',
+        playlistId: 'playlist-abc',
+        videoId: 'video-123',
       },
-    });
-  });
-
-  it('should return empty route for unknown type', () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    const video = {
-      id: '123',
-      type: 'unknown',
-      title: 'Test Unknown',
-    } as TransformedMediaItem;
-
-    expect(generateVideoDetailRoute(video)).toEqual({
-      to: '',
-      params: {},
     });
   });
 });
