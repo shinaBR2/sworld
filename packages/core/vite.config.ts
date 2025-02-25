8bf9a9165194681a8ef08f4448a656641f70c390
vuong <shinabr2@gmail.com>
2025-02-25 16:26:25 +0700

    config only

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['src/graphql/**'],
+      exclude: ['src/graphql/**', 'dist/**', 'codegen.ts', 'tsup.config.ts', 'vite.config.ts', '.eslintrc.js'],
     },
   },
 });
