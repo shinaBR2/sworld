484530390289c3772312106937a32cea5a34813f
vuong <shinabr2@gmail.com>
2025-02-26 17:56:57 +0700

    improve type

--- packages/ui/src/watch/history-page/types.ts
+++ packages/ui/src/watch/history-page/types.ts
@@ -1,22 +1,7 @@
 import { useLoadHistory } from 'core/watch/query-hooks/history';
-import { MediaType } from 'core/watch/query-hooks';
 import { RequiredLinkComponent } from '../videos/types';
 
-interface HistoryVideo {
-  id: string;
-  type: MediaType;
-  title: string;
-  slug: string;
-  source: string;
-  thumbnailUrl: string;
-  duration: number;
-  user: {
-    username: string;
-  };
-  progressSeconds?: number;
-  createdAt: string;
-  playlist?: ReturnType<typeof useLoadHistory>['videos'][number]['playlist'];
-}
+type HistoryVideo = ReturnType<typeof useLoadHistory>['videos'][number];
 
 interface HistoryContainerProps extends Omit<RequiredLinkComponent, 'linkProps'> {
   videos: HistoryVideo[];
