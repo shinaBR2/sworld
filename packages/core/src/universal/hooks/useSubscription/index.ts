da0479906f5799b3eb0a295d41dcdc3daae5520a
vuong <shinabr2@gmail.com>
2025-03-25 21:37:04 +0700

    export interface

--- packages/core/src/universal/hooks/useSubscription/index.ts
+++ packages/core/src/universal/hooks/useSubscription/index.ts
@@ -10,7 +10,7 @@ interface ConnectionInfo {
   subscriptionId: string;
 }
 
-interface SubscriptionParams {
+export interface SubscriptionParams {
   hasuraUrl: string;
   query: string;
   variables?: Record<string, unknown>;
