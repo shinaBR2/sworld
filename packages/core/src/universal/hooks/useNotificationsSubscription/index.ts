dced6d47766f00ac33e0bf8ac6890a475457a62a
vuong <shinabr2@gmail.com>
2025-03-25 21:39:59 +0700

    fix code

---
+++ packages/core/src/universal/hooks/useNotificationsSubscription/index.ts
@@ -0,0 +1,35 @@
+import { graphql } from '../../../graphql';
+import { NotificationsSubscription } from '../../../graphql/graphql';
+import { useAuthContext } from '../../../providers/auth';
+import { useSubscription } from '../useSubscription';
+
+const NOTIFICATIONS_SUBSCRIPTION = graphql(/* GraphQL */ `
+  subscription Notifications {
+    notifications {
+      id
+      entityId
+      entityType
+      type
+      readAt
+      message
+      link
+      metadata
+      title
+    }
+  }
+`);
+
+export function useNotificationsSubscription(url: string) {
+  const { isSignedIn } = useAuthContext();
+  const subscription = useSubscription<NotificationsSubscription>({
+    hasuraUrl: url,
+    query: NOTIFICATIONS_SUBSCRIPTION.toString(),
+    disabled: !isSignedIn,
+  });
+
+  return {
+    data: subscription.isLoading ? null : subscription.data?.notifications || [],
+    isLoading: subscription.isLoading,
+    error: subscription.error,
+  };
+}
