76d5b8d3c6150f74c6a3153d8d37a47e60dd06f3
vuong <shinabr2@gmail.com>
2025-04-13 16:03:19 +0700

    build(vite): configure server and preview ports and hosts
    This commit adds explicit configurations for the server and preview ports and hosts in the Vite configuration file. The server is set to run on port 3000 and the preview on port 4000, both bound to '0.0.0.0' to ensure accessibility across the network.

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,8 +1,16 @@
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://github.com/vitejs/vite/issues/5308#issuecomment-1010652389
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: 4000,
+    host: '0.0.0.0',
+  },
   plugins: [viteCommonjs(), react()],
 });
