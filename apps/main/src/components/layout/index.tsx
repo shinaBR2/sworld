82c6cb1bde915bbfa381a31d23dab9172b48fd45
vuong <shinabr2@gmail.com>
2025-04-14 20:34:42 +0700

    feat(layout): add header component with user authentication
    Introduce a new Header component to the layout, integrating user authentication functionality. The header displays a user avatar if logged in, otherwise shows a default icon. This enhances the user experience by providing a clear authentication state and interaction point.

--- apps/main/src/components/layout/index.tsx
+++ apps/main/src/components/layout/index.tsx
@@ -1,5 +1,7 @@
 import { FullWidthContainer } from 'ui/universal/containers/full-width';
 import React from 'react';
+import { Header } from 'ui/main/header';
+import { useAuthContext } from 'core/providers/auth';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -7,8 +9,21 @@ interface LayoutProps {
 
 const Layout = (props: LayoutProps) => {
   const { children } = props;
+  const { user, signIn } = useAuthContext();
+  const onProfileClick = () => {
+    if (user) {
+      return;
+    } else {
+      signIn();
+    }
+  };
 
-  return <FullWidthContainer>{children}</FullWidthContainer>;
+  return (
+    <FullWidthContainer>
+      <Header user={user} onProfileClick={onProfileClick} />
+      {children}
+    </FullWidthContainer>
+  );
 };
 
 export { Layout };
