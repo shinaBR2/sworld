16eecabd8c31a339c649c3aa5d691ffbca8cb895
vuong <shinabr2@gmail.com>
2025-06-30 18:17:07 +0700

    progress

---
+++ hasura/migrations/sworld/1751281966169_alter_table_public_reading_progresses_add_column_percentage/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."reading_progresses" add column "percentage" integer
+--  not null;
