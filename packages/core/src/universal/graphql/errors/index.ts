2a3ca35afe8c34169ca74a94973520ad97eb7ec2
ShinaBR2 <shinabr2@gmail.com>
2025-08-24 19:53:11 +0700

    fix logic

---
+++ packages/core/src/universal/graphql/errors/index.ts
@@ -0,0 +1,13 @@
+const graphqlErrors = (error: any) => {
+  const errors = error?.response?.errors;
+  return Array.isArray(errors)
+    ? errors.map((error: any) => error?.extensions?.code)
+    : [];
+};
+
+const isTokenExpired = (error: any) => {
+  const errorCodes = graphqlErrors(error);
+  return errorCodes.includes("invalid-jwt");
+};
+
+export { isTokenExpired };
