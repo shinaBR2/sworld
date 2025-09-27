34c71353f5b99d63569a26696f6b6ab8eb3a40c0
ShinaBR2 <shinabr2@gmail.com>
2025-09-27 20:52:50 +0700

    fix comment

--- packages/core/src/universal/errors/errorCodes/index.ts
+++ packages/core/src/universal/errors/errorCodes/index.ts
@@ -1,23 +1,23 @@
 import { AUTH_ERROR_CODES, AUTH_ERRORS } from './auth';
-import { BUSINESS_ERRORS } from './business';
+import { BUSINESS_ERROR_CODES, BUSINESS_ERRORS } from './business';
 import { DATABASE_ERROR_CODES, DATABASE_ERRORS } from './database';
 import { EXTERNAL_ERROR_CODES, EXTERNAL_ERRORS } from './external';
 import {
   HASURA_NATIVE_ERROR_CODES,
   HASURA_NATIVE_ERRORS,
 } from './hasuraNative';
-import { NETWORK_ERRORS } from './network';
+import { NETWORK_ERROR_CODES, NETWORK_ERRORS } from './network';
 import { SYSTEM_ERROR_CODES, SYSTEM_ERRORS } from './system';
-import { VALIDATION_ERRORS } from './validation';
+import { VALIDATION_ERROR_CODES, VALIDATION_ERRORS } from './validation';
 
 const ERROR_CODES = {
   ...SYSTEM_ERROR_CODES,
   ...AUTH_ERROR_CODES,
   ...DATABASE_ERROR_CODES,
   ...EXTERNAL_ERROR_CODES,
-  ...BUSINESS_ERRORS,
-  ...NETWORK_ERRORS,
-  ...VALIDATION_ERRORS,
+  ...BUSINESS_ERROR_CODES,
+  ...NETWORK_ERROR_CODES,
+  ...VALIDATION_ERROR_CODES,
   ...HASURA_NATIVE_ERROR_CODES,
 };
 
