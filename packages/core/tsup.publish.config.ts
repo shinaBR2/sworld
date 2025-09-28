77f016a989c5ee2ddcc4251459b4920f2bf5592c
ShinaBR2 <shinabr2@gmail.com>
2025-09-28 12:06:40 +0700

    fix: support dts

---
+++ packages/core/tsup.publish.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  dts: true, // ONLY CHANGE
+  sourcemap: true,
+  entry: [
+    './src/**/*.ts?(x)',
+    '!./src/**/*.test.ts?(x)',
+    '!./src/**/*.stories.ts?(x)',
+  ],
+  format: ['esm'],
+  outDir: 'dist/',
+  treeshake: true,
+  splitting: true,
+  shims: true,
+  external: ['react'],
+});
